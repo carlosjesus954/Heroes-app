27a73145430e9cb5088a20373f8084e76774c9b8
Carlos jesus <carlosjesus.moralespica2000@gmail.com>
2024-01-11 21:01:38 +0100

    Fixeando un problema con los guards

--- src/app/auth/guards/public.guard.ts
+++ src/app/auth/guards/public.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, Router, UrlSegment,  RouterStateSnapshot, } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class PublicGuard implements CanMatch, CanActivate {
@@ -14,7 +14,8 @@ export class PublicGuard implements CanMatch, CanActivate {
       tap( isAuthenticated => {
 
         if(isAuthenticated) this.router.navigate(['/heroes'])
-      } )
+      } ),
+    map(isAuthenticated => !isAuthenticated)
     )
   }
 
